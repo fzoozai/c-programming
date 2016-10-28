f6f78a0e00746d07c0842830157ca864a8e2c67e
fzoozai <fabian.soosaithasan@gmail.com>
2016-10-28 22:29:25 +0200

    Initial Commit

---
+++ c-programming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-programming
+//
+//  Created by fzoozai on 28/10/2016.
+//  Copyright © 2016 fzoozai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
