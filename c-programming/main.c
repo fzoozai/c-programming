7180a908fc3718aab96f8501ae07be40f2dc3605
fzoozai <fabian.soosaithasan@gmail.com>
2016-10-28 22:35:00 +0200

    first commit

--- c-programming/main.c
+++ c-programming/main.c
@@ -8,8 +8,8 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Hello, The Flash!\n");
     return 0;
 }
